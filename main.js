c53541fe823424756883da60633ba6db91f3b4a9
sanafatmagithub <sanaafatma111@gmail.com>
2023-07-16 00:58:43 +0530

    added delete button

--- main.js
+++ main.js
@@ -20,30 +20,41 @@ function onSubmit(e) {
     setTimeout(() => msg.remove(), 3000);
   } else {
     // Create new list item with user
-    const li = document.createElement('li');
+    let li = document.createElement('li');
 
     // Add text node with input values
     li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
-
-    // Append to ul
+   // Append to ul
     users.appendChild(li);
 
-    // Clear fields
-    // nameInput.value = '';
-    // emailInput.value = '';
-    }
-  
   //  localStorage.setItem('userDetailsName', nameInput.value);
   //  localStorage.setItem('userEmail', emailInput.value);
-   let name = nameInput.value;
-   let email = emailInput.value;
+  let myObj = {
+    name: nameInput.value,
+    email: emailInput.value,
+  };
+  let UserDetails_serialize = JSON.stringify(myObj);
+  localStorage.setItem(emailInput.value, UserDetails_serialize);
 
-   let myObj={
-     name: name,
-     email:email
-   };
-   
-   localStorage.setItem(myObj.email, JSON.stringify(myObj));
-  //  showUserOnScreen(myObj)
- 
+  // Clear fields
+  nameInput.value = "";
+  emailInput.value = "";
+
+  //create a delete button
+  const deletebtn = document.createElement("input");
+  deletebtn.type = "button";
+  deletebtn.value = "Delete";
+  deletebtn.classList = "deleteBtn";
+   li.appendChild(deletebtn);
+
+  deletebtn.onclick = function() {
+   localStorage.removeItem(myObj.email);
+   userList.removeChild(li);
+  };
+
 }
+}
+
+
+
+
